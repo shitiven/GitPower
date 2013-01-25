fab13284d08f72ba5d5f507d3de332da7139678c
shitiven <shitiven@gmail.com>
2013-01-25 21:32:51 +0800

    test

--- Common/hook/hooks/post-update.py
+++ Common/hook/hooks/post-update.py
@@ -9,17 +9,17 @@ REPOS_PATH = "/home/git/repositories"
 PAGES_PATH = "/home/git/pages"
 
 def is_gp_pages():
-	'''check head is gp-pages branch'''
+    '''check head is gp-pages branch'''
 
-	global head
-	gp_pages_re = re.compile("heads/gp-pages")
+    global head
+    gp_pages_re = re.compile("heads/gp-pages")
     if gp_pages_re.search(head) is not None:
         return True
     return False
 
 
 def get_repo_name(rename = True):
-	'''get project name and format with {username}_{project_name}'''
+    '''get project name and format with {username}_{project_name}'''
 
     name = os.getcwd()
     name = name.replace(REPOS_PATH+"/","")
@@ -29,20 +29,20 @@ def get_repo_name(rename = True):
 
 
 def update_pages(repo_name):
-	pages_dir = PAGES_PATH+"/"+repo_name.replace(".git","").lower()
+    pages_dir = PAGES_PATH+"/"+repo_name.replace(".git","").lower()
 
-	#if pages exits git pull to update else clone new pages
-	if os.path.isdir(pages_dir):
-		os.execvp('git', ['--git-dir',pages_dir+"/.git",'--work-tree',pages_dir,"pull"])
-	else:
-		os.execvp('git', ['clone', os.getcwd(), '-b', 'gp-pages', pages_dir])
+    #if pages exits git pull to update else clone new pages
+    if os.path.isdir(pages_dir):
+        os.execvp('git', ['--git-dir',pages_dir+"/.git",'--work-tree',pages_dir,"pull"])
+    else:
+        os.execvp('git', ['clone', os.getcwd(), '-b', 'gp-pages', pages_dir])
 
 
 if __name__ == '__main___':
-	repo_name = get_repo_name(rename=False)
+    repo_name = get_repo_name(rename=False)
 
-	#if is gp-pages branch then update
-	if is_gp_pages:
-		update_pages(repo_name)
-		
-	sys.exits(1)
+    #if is gp-pages branch then update
+    if is_gp_pages:
+        update_pages(repo_name)
+        
+    sys.exits(1)
