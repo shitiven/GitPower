0fe0c7e5ea8edc3c1dbb4325f9175ad27720e314
shitiven <shitiven@gmail.com>
2013-01-29 11:04:13 +0800

    add branchpermission file

---
+++ Depot/controllers/admin/viewBranchPermission.py
@@ -0,0 +1,48 @@
+# encoding: utf-8
+
+from Common import *
+from Depot.decorators import repo_access_required
+from Depot.models import BranchPermission
+
+
+@repo_access_required("owner")
+@csrf_protect
+def branch_permission(request, username, repo_name):
+
+    if request.method == "POST":
+        branch = request.POST.get("branch", None)
+        member = request.POST.get("member", None)
+        try:
+            member = User.objects.get(username=member)
+        except User.DoesNotExsit:
+            messages.error(request, "成员不存在")
+
+        if not member in request.repo.team_writers:
+            messages.error(request, "该成员不在可写权限组内")
+
+        obj, created = BranchPermission.objects.get_or_create(branch=branch, repo=request.repo)
+        obj.users.add(member)
+
+        messages.success(request, "权限指定成功")
+
+        return HttpResponseRedirect(reverse("repo_branch_permission", args=[username, repo_name]))
+
+    request.context.update({
+        "git_repo" : request.repo.repo()
+    })
+
+    return render("repo/admin/branch_permission.html", request, context=request.context)
+
+@repo_access_required("owner")
+@csrf_protect
+def branch_permission_remove(request, username, repo_name):
+
+    if request.method == "POST":
+        branch = request.POST.get("branch")
+        member = User.objects.get(username=username)
+        obj = BranchPermission.objects.get(repo=request.repo, branch=branch)
+        obj.users.remove(member)
+
+        messages.success(request, "移除成功")
+
+        return HttpResponseRedirect(reverse("repo_branch_permission", args=[username, repo_name]))
